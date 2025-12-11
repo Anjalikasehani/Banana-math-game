d6bf4a58366944acb35931c4a81b7923304996da
Anjalikasehani <anjalikasehani029@gmail.com>
2025-12-11 10:01:33 +0530

    Implemented the game

--- game.js
+++ game.js
@@ -5,28 +5,37 @@ let correctAnswer = null;
 let score = 0;
 let level = 1;
 
-let timer= null;
+let timer = null;
 let timeLeft = 0;
 
-//load first question
 
-window.onload =() => {
-  loadBananaQuestion();
+window.onload = () => {
+  score = Number(window.currentScore || 0);
+  level = Number(window.currentLevel || 1);
 
-  document.getElementById("username").innerText = data.username;
-  document.getElementById("score").innerText = data.score;
-  document.getElementById("level").innerText = data.level;
   
-  score = Number(sessionStorage.getItem("score")) || 0;
-  level = Number(sessionStorage.getItem("level")) || 1;
-
  
+  document.getElementById("score").innerText = score;
+  document.getElementById("level").innerText = level;
+ 
+  creatNumberButtons();
+  loadBananaQuestion();
+}
 
-    loadBananaQuestion();
+function creatNumberButtons(){
+  let container = document.getElementById("numberButtons");
+  container.innerHTML = "";
+
+  for(let i = 0; i <=9; i++){
+    let btn = document.createElement("button");
+    btn.innerText = i;
+    btn.addEventListener("click",() => handleAnswer(i));
+    container.appendChild(btn);
+  }
 }
 //start countdown timer
 function startTimer(){
-  timeLeft=20 + (level - 1)*5;
+  timeLeft=60 + (level - 1)*5;
   document.getElementById("timeLeft").innerText = timeLeft;
 
   clearInterval(timer);
@@ -38,7 +47,7 @@ function startTimer(){
     if(timeLeft <= 0){
       clearInterval(timer);
       alert("Time's up!");
-      document.getElementById("submitBtn").style.display = "none";
+      
       document.getElementById("nextBtn").style.display = "inline-block";
       
     }
@@ -48,57 +57,60 @@ function startTimer(){
 }
 //load question from Banana API
 async function loadBananaQuestion() {
-   console.log("Loading Game..");
+  
 
    startTimer();
 
    const img = document.getElementById("questionImage");
    img.src = "";
-  try {
+   
+try{
     const response = await fetch("https://marcconrad.com/uob/banana/api.php");
     const data = await response.json();
 
     
-    if (data.question) {
+  
         img.src = data.question;
         correctAnswer = Number(data.solution);
-        
-    }
-
-    //reset buttons
-     document.getElementById("submitBtn").style.display = "inline-block";
-    document.getElementById("nextBtn").style.display = "none";
-    document.getElementById("answerInput").value = "";
-    
-  } catch (error) {
-    
-    alert("Try Again");
-  }
+        console.log("correct answer", correctAnswer);
+} catch (e){
+  alert("Error!Try again");
+  return;
 }
-//submit answer
-document.getElementById("submitBtn").addEventListener("click", () => {
+   
  
-  const userAns =Number(document.getElementById("answerInput").value);
-  
-  if(userAns === correctAnswer) {
-    alert("Correct!");
+    document.getElementById("nextBtn").style.display = "none";
+    
+}
+
+function handleAnswer(userAns){
+  clearInterval(timer);
+
+//submit answer
+
+  if(userAns === correctAnswer){
+    alert("correct");
     score++;
     document.getElementById("score").innerText = score;
 
-   sessionStorage.setItem("score", score);
+    if(score >= 5){
+      score = 0;
+      level++;
+       alert("Level up!");
+      document.getElementById("level").innerText = level;
+     
+    }
 
-   if(score % 5 === 0){
-    level++;
-    document.getElementById("level").innerText = level;
-    sessionStorage.setItem("level", level);
-    alert("Level up!");
-   }
-   loadBananaQuestion();
   }else{
-    alert("Wrong!Correct answer is:" + correctAnswer);
+    alert("Incorrect!correct answer was:" + correctAnswer);
+    score = Math.max(0,score - 1);
+    document.getElementById("score").innerText = score;
   }
- 
-});
+  
+  document.getElementById("nextBtn").style.display = "inline-block";
+}
+  
+
 //next question
 document.getElementById("nextBtn").addEventListener("click", () => {
 
